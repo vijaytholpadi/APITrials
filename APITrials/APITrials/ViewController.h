feb9b743d426675eabba6e67a8f7201678402335
Vijay Tholpadi <vijay@thegeekprojekt.com>
2016-01-01 11:19:43 +0530

    Initial project/workspace creation

---
+++ APITrials/APITrials/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APITrials
+//
+//  Created by Vijay Tholpadi on 1/1/16.
+//  Copyright © 2016 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ APITrials/APITrials/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  APITrials
+//
+//  Created by Vijay Tholpadi on 1/1/16.
+//  Copyright © 2016 TheGeekProjekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
